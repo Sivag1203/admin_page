182ca7ae4f9c9999bb966fa88195ecabf3ae5e64
Sivaganesh Natarajavel <sivaganeshnatarajavel@gmail.com>
2024-01-27 09:26:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import AdminLogin from './AdminLogin';
+import Dashboard from './Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/admin-login" element={<AdminLogin />} />
+        <Route path="/Dash-board" element={<Dashboard />} />
+        <Route path="*" element={<Navigate to="/admin-login" />} />
+      </Routes>
+    </Router>
   );
 }
 
